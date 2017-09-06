13ca8db4cf30ce9b4dfab1d396c0c7b5caa4c89e
Eric Gjertsen <ericgj72@gmail.com>
2017-09-06 12:06:29 -0400

    initial commit

---
+++ src/Snackbar.elm
@@ -0,0 +1,222 @@
+module Snackbar exposing
+    ( Model
+    , Notification
+    , Msg
+    , empty
+    , defaultUpdateConfig
+    , emptyConfig
+    , push
+    , update
+    )
+
+import Time
+import Task exposing (Task)
+import Process
+import Html exposing (Html)
+import Html.Events
+
+-- STACK
+
+type Model a 
+    = Empty
+    | TransitioningIn (a, Float) (List (a, Float))
+    | Active (a, Float) (List (a, Float))
+    | TransitioningOut (a, Float) (List (a, Float))
+
+    
+empty : Model a
+empty = 
+    Empty
+    
+    
+type Msg 
+    = Activate
+    | TransitionOut
+    | Pop
+
+
+type UpdateConfig msg =
+    UpdateConfig
+        { updateMsg : Msg -> msg
+        , delayTransitioningIn : Float
+        , delayTransitioningOut : Float
+        }
+
+defaultUpdateConfig : (Msg -> msg) -> UpdateConfig msg
+defaultUpdateConfig tagger =
+    UpdateConfig
+        { updateMsg = tagger
+        , delayTransitioningIn = 0
+        , delayTransitioningOut = 2000
+        }
+
+update : UpdateConfig msg -> Msg -> Model a -> ( Model a, Cmd msg )
+update (UpdateConfig c) msg model =
+    let
+        map tagger (m,cmd) =
+            (m, Cmd.map tagger cmd)
+    in
+        case msg of
+            Activate ->
+                activate model 
+                    |> map c.updateMsg
+            
+            TransitionOut ->
+                transitionOut c.delayTransitioningOut model 
+                    |> map c.updateMsg
+            
+            Pop ->
+                pop c.delayTransitioningIn model 
+                    |> map c.updateMsg
+
+
+push : UpdateConfig msg -> Float -> a -> Model a -> (Model a, Cmd msg)    
+push (UpdateConfig c) timeout a model =
+    case model of
+        Empty ->
+            ( TransitioningIn (a, timeout) []
+            , delay Activate c.delayTransitioningIn |> Cmd.map c.updateMsg
+            )
+            
+        TransitioningIn current others ->
+            ( TransitioningIn current ((a, timeout) :: others)
+            , Cmd.none
+            )    
+        
+        Active current others ->
+            ( Active current ((a, timeout) :: others)
+            , Cmd.none
+            )
+            
+        TransitioningOut current others ->
+            ( TransitioningOut current ((a, timeout) :: others)
+            , Cmd.none
+            )    
+            
+
+activate : Model a -> (Model a, Cmd Msg)
+activate model =
+    case model of
+        TransitioningIn (current, timeout) others ->
+            ( Active (current, timeout) others
+            , delay TransitionOut timeout
+            )
+            
+        _ ->
+            ( model
+            , Cmd.none
+            )
+            
+transitionOut : Float -> Model a -> (Model a, Cmd Msg)
+transitionOut timeout model =
+    case model of
+        Active current others ->
+            ( TransitioningOut current others
+            , delay Pop timeout
+            )
+            
+        _ ->
+            ( model
+            , Cmd.none
+            )
+            
+pop : Float -> Model a -> ( Model a, Cmd Msg )
+pop timeout model =
+    case model of        
+        TransitioningOut _ (next :: others) ->
+            ( TransitioningIn next others
+            , delay Activate timeout
+            )
+            
+        TransitioningOut _ [] ->
+            ( Empty
+            , Cmd.none
+            )
+            
+        _ ->
+            ( model
+            , Cmd.none
+            )
+
+            
+-- NOTIFICATION
+     
+type Config msg =
+    Config
+        { messageAttributes : List (Html.Attribute msg)
+        , actionAttributes : List (Html.Attribute msg)
+        , transitioningInAttributes : List (Html.Attribute msg)
+        , activeAttributes : List (Html.Attribute msg)
+        , transitioningOutAttributes : List (Html.Attribute msg)
+        }
+
+type alias Notification msg =
+    { message : String
+    , action : msg
+    , actionText : String
+    }
+    
+emptyConfig : Config msg
+emptyConfig =
+    Config
+        { messageAttributes = []
+        , actionAttributes = []
+        , transitioningInAttributes = []
+        , activeAttributes = []
+        , transitioningOutAttributes = []
+        }
+
+view : Config msg -> Model (Notification msg) -> Html msg
+view (Config c) model =
+    case model of
+        Empty ->
+            Html.text ""
+            
+        TransitioningIn (notif, _) _ ->
+            viewNotification 
+                { container = c.transitioningInAttributes
+                , message = c.messageAttributes
+                , action = c.actionAttributes
+                }                
+                notif
+            
+        Active (notif, _) _ ->
+            viewNotification 
+                { container = c.activeAttributes
+                , message = c.messageAttributes
+                , action = c.actionAttributes
+                }                
+                notif
+            
+        TransitioningOut (notif, _) _ ->
+            viewNotification 
+                { container = c.transitioningOutAttributes
+                , message = c.messageAttributes
+                , action = c.actionAttributes
+                }
+                notif
+
+                
+viewNotification : 
+    { container : List (Html.Attribute msg) 
+    , message : List (Html.Attribute msg)
+    , action : List (Html.Attribute msg)
+    }
+    -> Notification msg 
+    -> Html msg
+viewNotification attribs { message, action, actionText } =
+    Html.div attribs.container
+        [ Html.div attribs.message [ Html.text message ]
+        , Html.button 
+            ( Html.Events.onClick action :: attribs.action )
+            [ Html.text actionText ]
+        ]
+            
+
+-- UTILS
+         
+delay : msg -> Float -> Cmd msg
+delay msg timeout =
+    Process.sleep timeout
+        |> Task.perform (\_ -> msg)
+
